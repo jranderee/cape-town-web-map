489633117bd770079cf7d8dca2362591c8524e15
Joel Randeree <shopping4joel@icloud.com>
2025-12-31 17:40:27 +0200

    2nd commit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import geopandas as gpd
 import folium
 from streamlit_folium import st_folium
 import pandas as pd
+from shapely.geometry import Point
 
 
 shapefile_path = "Wards/Wards.shp"
@@ -10,6 +11,7 @@ gdf = gpd.read_file(shapefile_path)
 
 csv_path = "Fire_Stations.csv"
 points_df = pd.read_csv(csv_path)
+
 
 st.title("Cape Town Wards Map")
 
@@ -29,4 +31,15 @@ folium.GeoJson(
     )
 ).add_to(m)
 
+for index, row in points_df.iterrows():
+    folium.Marker(
+        location=[row["Y"], row["X"]],
+        popup=row["FIRE_STN_NAME"],
+        tooltip=row["FIRE_STN_NAME"]
+    ).add_to(m)
+
 st_folium(m,width=700, height=500)
+
+# DEBUG: Show column names and first few rows
+st.write("### CSV Columns:", points_df.columns.tolist())
+st.write("### First 5 rows:", points_df.head())
