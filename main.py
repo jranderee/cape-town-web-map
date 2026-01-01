2c2691e288d76dcfc57354ebf2e0d65741e6efae
Joel Randeree <shopping4joel@icloud.com>
2026-01-01 10:45:27 +0200

    Add drop down menu. Implement different markers based on drop down selection. Inject CSS to hide Ukranian flag on watermark

--- main.py
+++ main.py
@@ -2,21 +2,39 @@ import streamlit as st
 import geopandas as gpd
 import folium
 from streamlit_folium import st_folium
-import pandas as pd
-from shapely.geometry import Point
 
-
+#read shapefile for ward boundaries
 shapefile_path = "Wards/Wards.shp"
 gdf = gpd.read_file(shapefile_path)
 
-csv_path = "Fire_Stations.csv"
-points_df = pd.read_csv(csv_path)
+#read shapefile for fire stations
+fire_stations_path = "Fire_Stations/Fire_Stations.shp"
+fire_statins_df = gpd.read_file(fire_stations_path)
 
+#read shapefile for fire stations
+train_stations_path = "Railway_Stations/Railway_Stations.shp"
+train_statins_df = gpd.read_file(train_stations_path)
 
+#Read shapefile for myciti bus stops
+myciti_bus_path = "myciti_bus_stops/Integrated_rapid_transit_(IRT)_system_MyCiTi_Bus_Stops.shp"
+myciti_df = gpd.read_file(myciti_bus_path)
+
+#Create streamlit title
 st.title("Cape Town Wards Map")
 
+option = st.selectbox(
+    "Choose an option:",
+    ["None","Fire Stations", "Train Stations", "MyCiti Bus Stops"]
+)
+
+
+#Create folium map of cape town
 m = folium.Map(location=[-33.9249, 18.4241], zoom_start=10, tiles="CartoDB positron")
 
+# Inject CSS to hide the Ukrainian flag
+m.get_root().html.add_child(folium.Element('<style> .leaflet-attribution-flag { display: none !important; } </style>'))
+
+#Add ward boundaries to map
 folium.GeoJson(
     gdf,
     style_function=lambda feature: {
@@ -27,19 +45,43 @@ folium.GeoJson(
     },
     tooltip=folium.GeoJsonTooltip(
         fields=["WARD_NAME"],
+        aliases=["Ward"],
         localize=True
     )
 ).add_to(m)
 
-for index, row in points_df.iterrows():
-    folium.Marker(
-        location=[row["Y"], row["X"]],
-        popup=row["FIRE_STN_NAME"],
-        tooltip=row["FIRE_STN_NAME"]
-    ).add_to(m)
+match option:
+    case "Fire Stations":
+        #add fire stations to map
+        folium.GeoJson(
+            fire_statins_df,
+            tooltip=folium.GeoJsonTooltip(
+                fields=["FIRE_STN_N"],
+                aliases=["Fire Station Name:"],
+                localize=True
+            )
+        ).add_to(m)
 
+    case "Train Stations":
+        folium.GeoJson(
+            train_statins_df,
+            tooltip=folium.GeoJsonTooltip(
+                fields=["NAME"],
+                aliases=["Train Station Name:"],
+                localize=True
+            )
+            ).add_to(m)
+
+    case "MyCiti Bus Stops":
+        folium.GeoJson(
+            myciti_df,
+            tooltip=folium.GeoJsonTooltip(
+                fields=["STOP_NAME"],
+                aliases=["Bus Stop Name:"],
+                localize=True
+            )
+        ).add_to(m)
+
+
+#display map
 st_folium(m,width=700, height=500)
-
-# DEBUG: Show column names and first few rows
-st.write("### CSV Columns:", points_df.columns.tolist())
-st.write("### First 5 rows:", points_df.head())
